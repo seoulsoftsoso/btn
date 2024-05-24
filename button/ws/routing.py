5c63b83b24221a830450de6e74e792c04661d73f
sjyoondd <sj.yoon@seoul-soft.com>
2024-05-24 16:29:10 +0900

    repath mod

--- button/ws/routing.py
+++ button/ws/routing.py
@@ -5,6 +5,6 @@ from button.ws import consumers
 logger = logging.getLogger(__name__)
 
 websocket_urlpatterns = [
-    re_path(r'ws/landing/user/$', consumers.DashboardConsumer.as_asgi()),
+    re_path(r'ws/landing/user/$', consumers.MyConsumer.as_asgi()),
 ]
 logger.info('WebSocket URL 패턴 설정 완료')
