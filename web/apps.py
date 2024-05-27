3b7559cab467645a0a12360049339b548898fd9c
sjyoondd <sj.yoon@seoul-soft.com>
2024-05-27 09:35:36 +0900

    mongoã stream isten(int activated

--- web/apps.py
+++ web/apps.py
@@ -1,6 +1,12 @@
 from django.apps import AppConfig
 
+from button.ws import mongo_updates
 
+class YourAppConfig(AppConfig):
+    name = 'web'
+
+    def ready(self):
+        mongo_updates.listen_to_changes()
 class WebConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'web'
