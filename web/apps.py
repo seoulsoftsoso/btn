5fa1ba1eae05c73714bb7db2625383f2b3adba83
sjyoondd <sj.yoon@seoul-soft.com>
2024-05-28 11:09:37 +0900

    unnecessary setting removed

--- web/apps.py
+++ web/apps.py
@@ -6,6 +6,3 @@ class YourAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'web'
 
-    def ready(self):
-        from button.ws.mongo_updates import start_listening_to_changes
-        start_listening_to_changes()
