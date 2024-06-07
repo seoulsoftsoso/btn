a7ecb987cf24b8092bbfc1ab7d9aefb9c314a9db
mes service <mes_service@daum.net>
2024-06-07 17:06:40 +0900

    mqttServer.py 업데이트

--- utils/mqttServer.py
+++ utils/mqttServer.py
@@ -1,101 +1,63 @@
-from pymongo import MongoClient
-import paho.mqtt.client as mqtt
-import certifi
+from datetime import datetime
 
-def on_message(client, userdata, msg):
-    print(str(msg.payload.decode("utf-8")))
+import certifi
+import paho.mqtt.client as mqtt
+from pymongo import MongoClient
+from pytz import timezone
+
+DB_NAME = 'djangoConnectTest'
+COLLECTION = 'sen_gather'
+SERVER_URL = ('mongodb+srv://sj:1234@cluster0.ozlwsy4.mongodb.net/?retryWrites=true&w=majority&appName'
+              '=Cluster0')
 
 def on_connect(client, userdata, flags, rc):
-        try:
-            print(f"Connected with result code {rc}")
-        except Exception as e:
-            print(f"Error connecting to MQTT: {e}")
-class MqttServer:
-    def __init__(self, mqtt_host='127.0.0.1', mqtt_port=1883, topic='test'):
-        self.mongo_client = None
-        self.mqtt_client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
-        self.db_name = 'djangoConnectTest'
-        self.collection = 'sen_gather'
-        self.message = None
-        self.server_url = ('mongodb+srv://sj:1234@cluster0.ozlwsy4.mongodb.net/?retryWrites=true&w=majority&appName'
-                           '=Cluster0')
-        self.server_config = {
-            'host': mqtt_host,
-            'port': mqtt_port,
-            'topic': topic
-        }
-
-    def mqtt_config(self):
-        self.mqtt_client = self.mqtt_client.d
-        self.mqtt_client.on_connect = self.on_connect
-        self.mqtt_client.on_message = on_message
-
-    def insert(self, data):
-        try:
-            client = MongoClient(self.server_url, tlsCAFile=certifi.where())
-            db = client[self.db_name]
-            collection = db[self.collection]
-            x = collection.insert_one(data)
-            if x.acknowledged:
-                self.mqtt_client.publish(self.server_config['topic'], str(data))
-        except Exception as e:
-            print(f"Error inserting data: {e}")
-
-    def on_mqtt_message(self, data):
-        try:
-            print(f"Received message: {data}")
-            # self.insert(data)
-            self.message = data
-        except Exception as e:
-            print(f"Error handling message: {e}")
-
-    def init_mqtt(self):
-        try:
-            self.mqtt_client.on_connect = on_connect
-            self.mqtt_client.on_message = on_message
-            self.mqtt_client.connect(self.server_config['host'], self.server_config['port'])
-            self.mqtt_client.loop_start()
-        except Exception as e:
-            print(f"Error initializing MQTT: {e}")
-
-    def close(self):
-        try:
-            self.mqtt_client.loop_stop()
-            self.mqtt_client.disconnect()
-            if self.mongo_client:
-                self.mongo_client.close()
-        except Exception as e:
-            print(f"Error closing connections: {e}")
+    if rc == 0:
+        print("connected OK")
+    else:
+        print("Bad connection Returned code=", rc)
 
 
-def on_disconnect(self, client, userdata, rc):
-    print(rc)
+def on_disconnect(client, userdata, flags, rc=0):
+    print(str(rc))
 
-def on_subscribe(client, userdata, mid, granted_pos):
-    print(f'subscribed:{str(mid)}{str(granted_pos)}')
+def on_subscribe(client, userdata, mid, granted_qos):
+    print("subscribed: " + str(mid) + " " + str(granted_qos))
 
+def on_message(client, userdata, msg):
+    data = str(msg.payload.decode("utf-8"))
+    if data == 'hello':
+        print('mqttServer: hello')
+        mongo = MongoClient(SERVER_URL, tlsCAFile=certifi.where())
+        db = mongo[DB_NAME]
+        collection = db[COLLECTION]
+        res = collection.insert_one({
+            "c_date": datetime.now(timezone('Asia/Seoul')),
+            "con_id": 35,
+            "senid": 41,
+            "type": "gta",
+            "value": 90
+        })
+        if res.acknowledged:
+            client.publish('test', 'ack')
+        else:
+            client.publish('test', 'nack')
+        mongo.close()
+    else:
+        print('mqttServer: ' + data)
+
+def on_publish(client, userdata, mid):
+    print("In on_pub callback mid= ", mid)
 
 if __name__ == "__main__":
-    mqtt_server = MqttServer('127.0.0.1', 1883, 'test')
-    mqtt_subscribe = mqtt.Client()
-    mqtt_subscribe.connect('127.0.0.1', 1883)
-    mqtt_subscribe.on_message = on_message
-    mqtt_subscribe.on_connect = on_connect
-    mqtt_subscribe.on_disconnect = on_disconnect
-    mqtt_subscribe.on_subscribe = on_subscribe
-    mqtt_subscribe.subscribe('test', 1)
+    client = mqtt.Client()
+    client.on_connect = on_connect
+    client.on_disconnect = on_disconnect
+    client.on_subscribe = on_subscribe
+    client.on_message = on_message
+    client.on_publish = on_publish
+    client.connect('127.0.0.1', 1883)
+    client.subscribe('test', 1)
+    client.publish('test', 'hello')
+    client.loop_forever()
 
-    mqtt_subscribe.loop_forever()
 
-    mqtt_server.mqtt_config()
-    mqtt_server.init_mqtt()
-
-    mqtt_server.on_mqtt_message({
-        "c_date": "test",
-        "con_id": 35,
-        "senid": 41,
-        "type": "test",
-        "value": 32
-    })
-
-    mqtt_server.close()
