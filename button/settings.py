9976df8c18a062d4eca0042c2de586f4ec969101
sjyoondd <sj.yoon@seoul-soft.com>
2024-06-19 08:29:32 +0000

    csrf~~ prob

--- button/settings.py
+++ button/settings.py
@@ -16,6 +16,7 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+CSRF_TRUSTED_ORIGINS = ['https://118.44.218.236:7000']
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
