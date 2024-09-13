c5d52f0eef4d6174a61fdfb63ec1af99d795aab8
mes service <mes_service@daum.net>
2024-09-13 09:19:16 +0900

    message

---
+++ api/control/senControl.py
@@ -0,0 +1,36 @@
+from rest_framework import viewsets
+
+from api.models import BomMaster, SenControl
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import serializers
+from django.db import transaction
+from rest_framework.permissions import AllowAny
+
+
+class senControlSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SenControl
+        fields = '__all__'
+
+    def create(self, validated_data):
+        with transaction.atomic():
+            ret = SenControl.objects.create(**validated_data)
+            return ret
+
+
+class senControlViewSet(viewsets.ModelViewSet):
+    queryset = SenControl.objects.all()
+    serializer_class = senControlSerializer
+    http_method_names = ['post']
+    filter_backends = [DjangoFilterBackend]
+    read_only_fields = ['id']
+    permission_classes = [AllowAny]
+
+    def get_queryset(self):
+        return SenControl.objects.filter()
+
+    def create(self, request, *args, **kwargs):
+        part_code = BomMaster.objects.get(id=request.data['senid']).part_code
+        request.data['part_code'] = part_code
+
+        return super().create(request, *args, **kwargs)
