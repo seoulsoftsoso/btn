6ef6d4eaf8e1c8c336d4556cdd13da9d8e1d02d5
seoulsoftsoso <150002152+seoulsoftsoso@users.noreply.github.com>
2024-10-21 16:04:10 +0900

    .

--- button/ws/flutter/term_data_updates.py
+++ button/ws/flutter/term_data_updates.py
@@ -22,24 +22,16 @@ def listen_to_changes_flutter_term(conId):
                 value = latest_control.value
                 # 문자열이 "[0,0]"인 경우 패스
                 if value == "[0, 0]":
-                    print(f"Skipping relay_id {relay.id} because value is [0,0]")
                     continue
-                print(f"Latest SenControl ID for relay_id {relay.id}: {latest_control.id}")
-                print(f"Part Code: {latest_control.part_code}")
-                print(f"Sen ID: {relay.sen_id}")
 
                 # relay의 sen_id를 키로, 최신 control의 value를 값으로 저장
                 result[relay.sen_id] = value
-                print(f"Result for sen_id {relay.sen_id}: {result[relay.sen_id]}")
             elif latest_control.mode == 'RSV_CYC':
                 value = latest_control.value
 
                 result[relay.sen_id] = value
-                print(f"Result for sen_id {relay.sen_id}: {result[relay.sen_id]}")
-                print('RSV_CYC!!!!!')
                 continue
             else:
-                print('else!!!!!!!')
                 continue
     send_initial_term_data_flutter(result, conId)
 
