64d37bb7940af06f51b46867dc1c30024c0d2915
seoulsoftsoso <150002152+seoulsoftsoso@users.noreply.github.com>
2024-10-30 10:05:37 +0900

    csrf

--- button/settings.py
+++ button/settings.py
@@ -19,7 +19,7 @@ pymysql.install_as_MySQLdb()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-CSRF_TRUSTED_ORIGINS = ['https://118.44.218.236:7000']
+CSRF_TRUSTED_ORIGINS = ['https://118.44.218.236:7000','https://btn.seoul-soft.com:7000']
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
